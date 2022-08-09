79d60316575fffed00334fa2ab8ebe3e26aa8a01
Shun Iwashita <iwashunabc519@gmail.com>
2022-08-09 22:55:11 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
-import '../scss/general.scss';
-import '../scss/navbar-toggle.scss';
-import '../scss/section-page.scss';
+import './scss/general.scss';
+import './scss/navbar-toggle.scss';
+import './scss/section-page.scss';
+import './scss/mobile-structure.scss';
 
 import $ from 'jquery';
 
-console.log(navigator.userAgent);
+// console.log(navigator.userAgent);
 
-const isPhoneOrTablet = navigator.userAgent.search(/iPhone|Android|iPad/);
-console.log(isPhoneOrTablet !== -1);
+const isPhoneOrTablet = navigator.userAgent.search(/iPhone|Android|iPad/) !== -1;
+const responsive = isPhoneOrTablet ? 'touch' : 'click';
 
 //* Toggle switch - Darkmode || Lightmode
 const toggleMode = $('#toggle-mode input');
-toggleMode.on('click', function() {
+toggleMode.on(responsive, function() {
    $('#root').toggleClass('dark');
 })
 
 
 //* Navbar - Background color & display section
 const navbars = $('.navbar-nav');
-navbars.on('click', function() {
+navbars.on(responsive, function() {
    navbars.removeClass('checked');
    $(this).addClass('checked');
    const sectionId = $(this).attr('data-section-name');
    $('.page').addClass('hide');
    $(`[data-id~="${sectionId}"]`).removeClass('hide');
+})
+
+const navbar = $('#navbar');
+navbar.on(responsive, function() {
+   $(this).toggleClass('open')
 })
