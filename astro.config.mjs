530a1f39ec4669585795702a8b6eb721b8c0ef1d
Shun Iwashita <iwashunabc519@gmail.com>
2025-04-10 23:38:59 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,12 @@ import { imagetools } from 'vite-imagetools';
 
 dotenv.config();
 
+const BASE_URL = process.env.BASE_URL || '/';
+const PUBLIC_URL = BASE_URL.replace(/\/$/, '');
+
 // https://astro.build/config
 export default defineConfig({
-  base: process.env.BASE_URL || '/',
+  base: BASE_URL,
   output: 'static',
   outDir: './dist',
   vite: {
@@ -27,8 +30,38 @@ export default defineConfig({
     ],
     resolve: {
       alias: {
-        '@utils': resolve('./src/utils')
+        '@utils': resolve('./src/utils'),
+        '@fonts': resolve('./src/assets/fonts'),
+        '@images': resolve('./src/assets/images'),
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `$public-url: "${PUBLIC_URL}";`,
+        }
+      }
+    },
+    build: {
+      rollupOptions: {
+        output:  {
+          hashCharacters: 'hex',
+          assetFileNames: (assetInfo) => {
+            const prefix = "img_";
+            const imageFiles = /^.*\.(jpg|jpeg|webp|avif|png|gif)$/
+            const fontFiles = /^.*\.(woff|woff2|ttf)$/
+
+            console.log(assetInfo.names);
+            // if(assetInfo.names.match(imageFiles)) {
+            //   return `assets/images/${prefix}[hash][extname]`;
+            // } else if(assetInfo.names.match(fontFiles)) {
+            //   return `assets/fonts/[name]-[hash][extname]`;
+            // }
+            return `assets/[name]-[hash:26][extname]`;
+          }
+        }
       }
     }
   },
+  integrations: [],
 });
